ff64aebaa44a2826e1e9dea4c78bccaa8fa2ba17
Antonin Delpeuch <antonin@delpeuch.eu>
2020-07-01 23:17:36 +0200

    Migrate text transform and column addition operations

--- or-grel/src/main/java/org/openrefine/expr/functions/strings/ToTitlecase.java
+++ or-grel/src/main/java/org/openrefine/expr/functions/strings/ToTitlecase.java
@@ -40,6 +40,8 @@ import org.openrefine.grel.ControlFunctionRegistry;
 import org.openrefine.grel.PureFunction;
 
 public class ToTitlecase extends PureFunction {
+
+    private static final long serialVersionUID = 4173141031075676198L;
     final static private char[] delimiters = { ' ', '\t', '\r', '\n', '.' };
     
     @Override
