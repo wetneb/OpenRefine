b5975dfe5c60e33121364f3ab87b6d8943894cf1
Antonin Delpeuch <antonin@delpeuch.eu>
2022-12-09 14:01:47 +0100

    Avoid recomputing record boundaries when it is preserved by the operation

--- refine-workflow/src/main/java/org/openrefine/model/changes/CellChange.java
+++ refine-workflow/src/main/java/org/openrefine/model/changes/CellChange.java
@@ -36,10 +36,7 @@ package org.openrefine.model.changes;
 import java.io.Serializable;
 import java.util.Collections;
 
-import org.openrefine.model.Cell;
-import org.openrefine.model.GridState;
-import org.openrefine.model.Row;
-import org.openrefine.model.RowMapper;
+import org.openrefine.model.*;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -77,10 +74,11 @@ public class CellChange implements Change {
                     String.format("Column '%s' does not exist", columnName));
         }
         // set judgment id on recon if changed
-        return projectState.mapRows(mapFunction(index, row, newCellValue), projectState.getColumnModel());
+        ColumnModel columnModel = projectState.getColumnModel();
+        return projectState.mapRows(mapFunction(index, row, newCellValue, columnModel.getKeyColumnIndex()), columnModel);
     }
 
-    static protected RowMapper mapFunction(int cellIndex, long rowId, Serializable newCellValue) {
+    static protected RowMapper mapFunction(int cellIndex, long rowId, Serializable newCellValue, int keyColumnIndex) {
         return new RowMapper() {
 
             private static final long serialVersionUID = -5983834950609157341L;
@@ -94,6 +92,11 @@ public class CellChange implements Change {
                 } else {
                     return row;
                 }
+            }
+
+            @Override
+            public boolean preservesRecordStructure() {
+                return keyColumnIndex != cellIndex;
             }
         };
     }
