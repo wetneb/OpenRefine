de6adf9298076bd9ed3042d50888fbe5b1ba74bd
Antonin Delpeuch <antonin@delpeuch.eu>
2023-04-15 16:11:15 +0200

    WIP: importer migration to CloseableIterable

--- modules/refine-model/src/main/java/org/openrefine/importers/ImporterUtilities.java
+++ modules/refine-model/src/main/java/org/openrefine/importers/ImporterUtilities.java
@@ -33,16 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package org.openrefine.importers;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Serializable;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -211,6 +202,12 @@ public class ImporterUtilities {
                 progress.readingFile(fileSource, this.bytesRead);
 
                 return l;
+            }
+
+            @Override
+            public void close() throws IOException {
+                progress.endFile(fileSource, file.length());
+                super.close();
             }
         };
     }
